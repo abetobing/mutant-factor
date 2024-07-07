5f9741508cd12d60dfb7d488d4dde405cac94c0e
abetobing <abedzilla@gmail.com>
2024-07-07 16:19:24 +0700

    add tree that produce fruit (food resource)

---
+++ Assets/FoodProducer.cs
@@ -0,0 +1,34 @@
+using Entities;
+using UnityEngine;
+
+public class FoodProducer : MonoBehaviour
+{
+    private float _interval = 3f;
+    [SerializeField] private GameObject fruitPrefab;
+
+    private float _elapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _elapsed += Time.deltaTime;
+        if (_elapsed >= _interval)
+        {
+            _elapsed = 0;
+            Vector3 randomSpawnPoint = transform.position + Random.onUnitSphere + (Vector3.up * 2f);
+            var fruit = Instantiate(fruitPrefab);
+            fruit.transform.SetPositionAndRotation(randomSpawnPoint, transform.rotation);
+            fruit.GetComponent<Rigidbody>().AddForce(Random.insideUnitCircle.normalized);
+            fruit.GetComponent<FoodSource>().available = Random.Range(
+                fruit.GetComponent<FoodSource>().available / 3, fruit.GetComponent<FoodSource>().available
+            );
+            fruit.transform.localScale *= Random.Range(0.5f, 1f);
+            Debug.Log(fruit.GetComponent<FoodSource>().available);
+        }
+    }
+}
